cf7732f771851f142994c93d8971df52baa394eb
zzxn <zzxnhackman@gmail.com>
2019-12-16 16:57:54 +0800

    add view for changing password (service is still unimplemented, because the API design is wrong)

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Location } from '@angular/common';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-me',
@@ -14,7 +15,11 @@ export class MeComponent implements OnInit {
   group_id: string;
   tokenExpireDate: Date;
 
-  constructor(private authService: AuthService, private location: Location) {
+  errorMsg: string;
+  changePassFormGroup: FormGroup;
+  changingPassword = false;
+
+  constructor(private authService: AuthService, private location: Location, private fb: FormBuilder) {
     const token = this.authService.getToken();
     const expireTime = parseInt(localStorage.getItem('token_expire_time'), 10);
     const expireDate = new Date();
@@ -25,9 +30,66 @@ export class MeComponent implements OnInit {
     this.group_id = token.group_id;
     this.token = token.token;
     this.tokenExpireDate = expireDate;
+
+    this.changePassFormGroup = this.fb.group({
+      oldPassword: ['', [this.passwordValidator]],
+      password: ['', [this.passwordValidator]],
+      confirm: ['', [this.confirmValidator]]
+    });
   }
 
   ngOnInit() {
   }
 
+  submitChangePassForm(): void {
+    // tslint:disable-next-line:forin
+    for (const i in this.changePassFormGroup.controls) {
+      this.changePassFormGroup.controls[i].markAsDirty();
+      this.changePassFormGroup.controls[i].updateValueAndValidity();
+    }
+
+    this.changingPassword = true;
+
+    const oldPassword = this.changePassFormGroup.controls.oldPassword.value;
+    const newpassword = this.changePassFormGroup.controls.password.value;
+
+    this.changingPassword = false;
+    console.error('Unimplemented');
+    // this.authService.changePassword(oldPassword, newpassword)
+    //   .subscribe(
+    //     (tokenRes) => {
+    //       console.log('get normal response');
+    //     },
+    //     (errorMsg) => {
+    //       this.errorMsg = errorMsg;
+    //       this.changingPassword = false;
+    //     }
+    //   );
+  }
+
+  validateConfirmPassword(): void {
+    setTimeout(() => this.changePassFormGroup.controls.confirm.updateValueAndValidity());
+  }
+
+  confirmValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return {required: true};
+    } else if (control.value !== this.changePassFormGroup.controls.password.value) {
+      return {confirm: true, error: true};
+    }
+    return {};
+  };
+
+  passwordValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return {required: true};
+    } else if (control.value.toString().length < 5) {
+      return {short: true};
+    } else if (control.value.toString().length > 20) {
+      return {long: true};
+    } else if (/^[0-9]+$/.test(control.value.toString())) {
+      return {onlyNumber: true};
+    }
+    return {};
+  };
 }
